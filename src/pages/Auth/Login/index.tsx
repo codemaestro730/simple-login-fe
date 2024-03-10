023bffa5c338a8af62b47fc2e46116b5d969795c
codemaestro730 <secretsuperdev730@gmail.com>
2024-03-10 18:53:26 +0100

    feat: implement register page

---
+++ src/pages/Auth/Login/index.tsx
@@ -0,0 +1,3 @@
+export const Login = () => {
+  return <div>123</div>;
+};
---
+++ src/pages/Dashboard/index.tsx
@@ -0,0 +1,7 @@
+export const Dashboard = () => {
+  return (
+    <div className="flex-center h-full w-full text-center text-xl">
+      Welcome to the application.
+    </div>
+  );
+};
